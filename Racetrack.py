93bd3f5e6ec00b85e2b3373770ed1fcb35acd572
Ben Lam <benlam@Bens-MBP.ht.home>
2025-03-03 17:45:30 -0500

    finsihed off env

--- Racetrack.py
+++ Racetrack.py
@@ -17,13 +17,13 @@ class Racetrack:
 
 
 
-
     # reset car position to random and velocity to 0 0
     def reset(self):
         self.position = random.choice(self.start_positions)
         self.velocity = [0,0]
 
         return self.position, self.velocity
+
 
 
     # checks to see if car is in bounds and is not on a wall
@@ -35,55 +35,53 @@ class Racetrack:
 
 
 
-
-    # for checking if we have crossed the finish line it is a bit more tricky,
-    # we have to make sure that none of the intermediate positions are on the finish line
+    # check to see if car is on or crosses finish line
     def crosses_finish_line(self, y, x, vy, vx):
-
-        # first check if we are on the finish line
         if [y,x] in self.finish_positions:
             return True
 
-        # then check to see if we have overshot the finish
+        # calculate intermediate positions to see if we cross finish
         steps = max(abs(vx), abs(vy))
-        for i in range(1, steps + 1):  # check each intermediate position
+        for i in range(1, steps + 1):
             intermediate_y = y + round(i * vy / steps)
             intermediate_x = x + round(i * vx / steps)
-            if (intermediate_y, intermediate_x) in self.finish_positions:
-                return True  # finish line detected
+            if [intermediate_y, intermediate_x] in self.finish_positions:
+                return True
 
-        return False  # no crossing detected
-
-
+        return False
 
 
-    # next we need to determine what happens when we take a step
-    # for each step we will have to return the state, the velocity, the reward, and whether or not we are done
+
+    '''
+    1. check for valid action
+    2. apply changes to velocity, clip velocity
+    3. check if we have crossed the finish line
+    4. check if we are out of bounds
+    5. update position
+    '''
     def step(self, action):
-        # check for funny business
         if action not in ACTION_SPACE:
             raise ValueError(f'Invalid action: {action}')
 
-        # there will be a 10% chance that the car will not accelerate
+        # 10% chance no change
         if random.random() < 0.1:
             action = (0,0)
 
-        # apply changes to velocity
         self.velocity[0] += action[0]
         self.velocity[1] += action[1]
 
-        # enforce speed limits
+        # clip speed
         max_speed = 5
         self.velocity[0] = max(-max_speed, min(max_speed, self.velocity[0]))
         self.velocity[1] = max(-max_speed, min(max_speed, self.velocity[1]))
 
-        # check if we have crossed the finish line
+        # check finish line
         if self.crosses_finish_line(self.position[0], self.position[1], self.velocity[0], self.velocity[1]):
-            print('FINSIH')
+            print('FINISH')
             return self.position, self.velocity, 100, True
 
-        # check head to see if we are out of bounds
-        new_y_position = self.position[0] - self.velocity[0] # minus to make it more intuitive what direction we are going in
+        # check out of bounds
+        new_y_position = self.position[0] - self.velocity[0] # minus to account for the fact that the y axis is flipped
         new_x_position = self.position[1] + self.velocity[1]
 
         if not self.is_valid_position(new_y_position, new_x_position):
@@ -91,11 +89,82 @@ class Racetrack:
             position, velocity = self.reset()
             return position, velocity, -20, False
 
-        # then if we are still in play then we will update the position
+        # if all checks pass then udpate position
         self.position = [new_y_position, new_x_position]
         return self.position, self.velocity, -1, False # impose small step penalty
 
+
+
+    # prints out current position of car and track
+    def print_track(self):
+        track_copy = [list(row) for row in self.track]
+        y, x = self.position
+        track_copy[y][x] = 'C' 
+
+        for row in track_copy:
+            print(''.join(row))
         
+
+
+# takes two space separated integers as input
+def get_user_action():
+    while True:
+        try:
+            # take input and turn into int list
+            action = tuple(map(int, input("enter vel command: ").split()))
+            print('\n')
+            if action in ACTION_SPACE: return action
+            else: print("Invalid action")
+        except ValueError: print("Invalid input")
+
+def visual_test():
+    tiny_track = [
+    "#####",
+    "#S  #",
+    "#  F#",
+    "#   #",
+    "#####"
+    ]
+
+    track1 = [
+        "################",
+        "#.............F#",
+        "#.............F#",
+        "#.............F#",
+        "#.............F#",
+        "#.............F#",
+        "#.............F#",
+        "#......#########",
+        "#......#########",
+        "#......#########",
+        "#......#########",
+        "#......#########",
+        "#SSSSSS#########",
+        "################"
+    ]
+
+
+
+    env = Racetrack(track1) 
+    print('start positions: ', env.start_positions)
+    print('finish positions: ', env.finish_positions)
+    done = False
+
+    while not done:
+        env.print_track()
+        action = get_user_action()  # Get user action
+        position, velocity, reward, done = env.step(action)
+
+        print(f'Position: {env.position}')
+        print(f"Velocity: {env.velocity}")
+        print(f'reward: {reward}')
+        print(done, "\n")
+
+
+
+if __name__ == "__main__":
+    visual_test()
+
 
 
 def testing_function():
@@ -153,63 +222,4 @@ def testing_function():
 
     # test step function
 
-def print_track(env):
-    track_copy = [list(row) for row in env.track]  # Create a copy of the track
-    y, x = env.position
-    track_copy[y][x] = 'C' 
-
-    # Print the updated racetrack
-    for row in track_copy:
-        print(''.join(row))
-
-
-
-
-
-# takes two space separated integers as input
-def get_user_action():
-    while True:
-        try:
-            # take input and turn into int list
-            action = tuple(map(int, input("enter vel command: ").split()))
-            print('\n')
-            if action in ACTION_SPACE:
-                return action
-            else:
-                print("Invalid action")
-        except ValueError:
-            print("Invalid input")
-
-def visual_test():
-    tiny_track = [
-    "#####",
-    "#S  #",
-    "#  F#",
-    "#   #",
-    "#####"
-    ]
-
-    env = Racetrack(tiny_track) 
-    print(env.finish_positions)
-
-    print_track(env)
-
-    done = False
-    print(done)
-
-    while not done:
-        action = get_user_action()  # Get user action
-        position, velocity, reward, done = env.step(action)
-
-        print_track(env)  # Update track with new position
-        print(f'Position: {env.position}')
-        print(f"Velocity: {env.velocity}")
-        print(f'reward: {reward}')
-        print(done, "\n")
-
-
-
-
-if __name__ == "__main__":
-    visual_test()
 
